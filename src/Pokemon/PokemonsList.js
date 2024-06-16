f5a2133ec580dcee3ec92dc54e46646be2058509
Rebecca <rebecca90469@gmail.com>
2024-06-16 19:01:02 -0400

    Added image rendering

--- src/Pokemon/PokemonsList.js
+++ src/Pokemon/PokemonsList.js
@@ -1,21 +1,39 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { PokemonContext } from './PokemonContext';
-import { listPokemons } from './listPokemons';
 
-const url = 'https://pokeapi.co/api/v2/pokemon';
+const url = 'https://pokeapi.co/api/v2/pokemon?limit=150&offset=0';
 
 const PokemonsList = () => {
   const { pokemons, capture, addPokemons } = useContext(PokemonContext);
+  const [pokemonImages, setPokemonImages] = useState([]);
 
   useEffect(() => {
-    const fetchPokemons = async () => {
-      const response = await fetch(url);
-      const data = await response.json();
-      addPokemons(data.results);
+    const fetchPokemonSprites = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const allPKMdata = data.results;
+        
+        addPokemons(data.results);
+
+        const imgUrls = [];
+
+        for (let i = 0; i < allPKMdata.length; i++) {
+          const pokemon = allPKMdata[i];
+          const imgResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
+          const imgData = await imgResponse.json();
+          const imageUrl = imgData.sprites.front_default;
+          imgUrls.push(imageUrl);
+        }
+
+        setPokemonImages(imgUrls);
+      } catch (error) {
+        console.error('Error fetching Pokémon data:', error);
+      }
     };
 
-    fetchPokemons();
+    fetchPokemonSprites();
   }, []);
 
   return (
@@ -23,15 +41,28 @@ const PokemonsList = () => {
       <h2>Pokemons List</h2>
 
       <table>
-        <tr>
-          <th>Pokemon</th>
-          <th>Capture</th>
-        </tr>
-        {listPokemons({
-          pokemons,
-          onClick: capture,
-          buttonLabel: '+',
-        })}
+        <thead>
+          <tr>
+            <th>Pokemon</th>
+            <th>Image</th>
+            <th>Capture</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pokemons.map((pokemon, index) => (
+            <tr key={pokemon.name}>
+              <td>{pokemon.name}</td>
+              <td>
+                {pokemonImages[index] && (
+                  <img src={pokemonImages[index]} alt={pokemon.name} style={{ width: '100px', height: '100px' }} />
+                )}
+              </td>
+              <td>
+                <button onClick={() => capture(pokemon)}>Capture</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
